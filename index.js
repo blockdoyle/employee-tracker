4fd8f80fd83001332560866c093cfbc3d84ff70c
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-02-18 17:29:58 -0500

    Refactor menu display and add callback to SQL commands

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const sqlc = require('./modules/sqlCommands');
-const inquirer = require('inquirer');
+const sqlc = require("./modules/sqlCommands");
+const inquirer = require("inquirer");
 
 const menuOptions = [
-    {
-        type: 'list',
-        name: 'menu',
-        message: 'What would you like to do?',
-        choices: [
-            'View All Departments',
-            'View All Roles',
-            'View All Employees',
-            'Add a Department',
-            'Add a Role',
-            'Add an Employee',
-            'Update an Employee Role',
-        ]
-    }
+  {
+    type: "list",
+    name: "menu",
+    message: "What would you like to do?",
+    choices: [
+      "View All Departments",
+      "View All Roles",
+      "View All Employees",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update an Employee Role",
+    ],
+  },
 ];
 
-// Start the application
-function init () {
-    inquirer.prompt(menuOptions).then((answers) => {
-        switch (answers.menu) {
-            case 'View All Departments':
-                sqlc.viewAllDepartments();
-                break;
-            case 'View All Roles':
-                sqlc.viewAllRoles();
-                break;
-            case 'View All Employees':
-                sqlc.viewAllEmployees();
-                break;
-            case 'Add a Department':
-                sqlc.addDepartment();
-                break;
-            case 'Add a Role':
-                sqlc.addRole();
-                break;
-            case 'Add an Employee':
-                sqlc.addEmployee();
-                break;
-            case 'Update an Employee Role':
-                sqlc.updateEmployeeRole();
-                break;
-        }
-    });
-}
+function displayMenu () {
+  inquirer.prompt(menuOptions).then((answers) => {
+    switch (answers.menu) {
+      case "View All Departments":
+        sqlc.viewAllDepartments(displayMenu);
+        break;
+      case "View All Roles":
+        sqlc.viewAllRoles(displayMenu);
+        break;
+      case "View All Employees":
+        sqlc.viewAllEmployees(displayMenu);
+        break;
+      case "Add a Department":
+        sqlc.addDepartment(displayMenu);
+        break;
+      case "Add a Role":
+        sqlc.addRole(displayMenu);
+        break;
+      case "Add an Employee":
+        sqlc.addEmployee(displayMenu);
+        break;
+      case "Update an Employee Role":
+        sqlc.updateEmployeeRole(displayMenu);
+        break;
+    }
+})};
 
-init();
+// Start the application
+displayMenu();
--- modules/sqlCommands.js
+++ modules/sqlCommands.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 // Create a connection to the database
 const db = mysql.createConnection(
@@ -12,13 +13,176 @@ const db = mysql.createConnection(
 );
 
 // View all departments
-function viewAllDepartments() {
-    db.query('SELECT * FROM department', function (err, res) {
-        if (err) throw err;
-        console.table(res);
+function viewAllDepartments(callback) {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(rows);
+        callback();
+    });
+}
+
+// view all roles
+function viewAllRoles(callback) {
+    const sql = `SELECT role.id, role.title, role.salary, department.name AS department_name FROM role JOIN department ON role.department_id = department.id`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(rows);
+        callback();
+    });
+}
+
+// view all employees
+function viewAllEmployees (callback) {
+    // employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(rows);
+        callback();
+    });
+}
+
+// add a department
+function addDepartment (callback) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the department?'
+        }
+    ]).then((answers) => {
+        const sql = `INSERT INTO department (name) VALUES (?)`;
+        db.query(sql, answers.name, (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Department added');
+            callback();
+        });
+    });
+}
+
+// add a job role
+function addRole (callback) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the role?'
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'What is the department id of the role?'
+        }
+    ]).then((answers) => {
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        db.query(sql, [answers.title, answers.salary, answers.department_id], (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Role added');
+            callback();
+        });
+    });
+}
+
+// add an employee
+function addEmployee (callback) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'What is the manager id of the employee?'
+        }
+    ]).then((answers) => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        db.query(sql, [answers.first_name, answers.last_name, answers.role_id, answers.manager_id], (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Employee added');
+            callback();
+        });
+    });
+}
+
+// update an employee role
+function updateEmployeeRole (callback) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'What is the id of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id of the employee?'
+        }
+    ]).then((answers) => {
+        const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+        db.query(sql, [answers.role_id, answers.employee_id], (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Employee role updated');
+            callback();
+        });
+    });
+}
+
+function exitServer () {
+    db.end(err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('MySQL connection closed');
+        }
     });
 }
 
 module.exports = {
     viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole,
+    exitServer,
 };
